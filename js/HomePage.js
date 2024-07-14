e1cd0850d1dc2e7fb4cdce6643c582a179cd460f
Rishika-jat <rishikajat03@gmail.com>
2024-07-14 08:06:23 +0530

    filter buttons

--- js/HomePage.js
+++ js/HomePage.js
@@ -3,6 +3,7 @@
 let menuBar = document.querySelector(".icon-menu");
 let sidebar = document.querySelector(".sidebar");
 let sidebar2 = document.querySelector(".sidebar2");
+let filterbtns = document.querySelector('.filter-btns')
 let side1 = document.querySelector(".side-1");
 let side2 = document.querySelector(".side-2");
 let side3 = document.querySelector(".side-3");
@@ -17,9 +18,18 @@ menuBar.addEventListener("click", function () {
     sidebar.style.display = "none";
     sidebar2.style.display = "block";
     currentStatusSide = false;
+    filterbtns.style.position="sticky";
+    filterbtns.sidebar.style.top="0px";
+    filterbtns.sidebar.style.left="0px";
+   
   } else if (currentStatusSide === false) {
     sidebar.style.display = "block";
     sidebar2.style.display = "none";
     currentStatusSide = true;
+    filterbtns.style.position="absolute";
+    filterbtns.sidebar.style.top="70px";
+    filterbtns.sidebar.style.left="250px";
+   
+
   }
 });
