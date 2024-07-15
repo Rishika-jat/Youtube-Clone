360f500ffb7c0ea471225df380c31ba5c6d93376
Rishika-jat <rishikajat03@gmail.com>
2024-07-15 23:08:00 +0530

    Add more thumbnail

--- js/HomePage.js
+++ js/HomePage.js
@@ -5,10 +5,10 @@ let sidebar = document.querySelector(".sidebar");
 let sidebar2 = document.querySelector(".sidebar2");
 let filterbtns = document.querySelector(".filter-btns");
 let container = document.querySelector(".container");
-let thumbnails = document.querySelectorAll('.thumbnail')
-let profile = document.querySelectorAll('.vid-profile');
-let bannerimg = document.querySelector('.banner-img')
-let timestmp = document.querySelectorAll('.time-stamp')
+let thumbnails = document.querySelectorAll(".thumbnail");
+let profile = document.querySelectorAll(".vid-profile");
+let bannerimg = document.querySelector(".banner-img");
+let timestmp = document.querySelectorAll(".time-stamp");
 let download = document.querySelector(".download");
 
 let currentStatusSide = true;
@@ -20,63 +20,74 @@ menuBar.addEventListener("click", function () {
 
     sidebar2.style.position = "absolute";
     filterbtns.style.left = "80px";
-   bannerimg.style.width="1400px"
+    bannerimg.style.width = "1400px";
 
     container.style.left = "80px";
-    container.style.width="1400px";
-   thumbnails.forEach(thumbnail=>{
-    thumbnail.style.width="335px"
-   })
-   profile.forEach(vidprofile=>{
-// vidprofile.style.gap="30px"
-vidprofile.style.width="335px"
-   })
-   timestmp.forEach(time=>{
-    time.style.left ="19vw";
-    time.style.top="9vw"
-   })
-   
-  
+    container.style.width = "1400px";
+    thumbnails.forEach((thumbnail) => {
+      thumbnail.style.width = "335px";
+    });
+    profile.forEach((vidprofile) => {
+      // vidprofile.style.gap="30px"
+      vidprofile.style.width = "335px";
+    });
+    timestmp.forEach((time) => {
+      time.style.left = "19vw";
+      time.style.top = "9vw";
+    });
   } else if (currentStatusSide === false) {
     sidebar.style.display = "block";
     sidebar2.style.display = "none";
     currentStatusSide = true;
     filterbtns.style.left = "250px";
-     bannerimg.style.width="81vwpx"
+    bannerimg.style.width = "81vwpx";
     container.style.position = "absolute";
     container.style.left = "260px";
-    container.style.width="90vw";
-    thumbnails.forEach(thumbnail=>{
-      thumbnail.style.width="26vw"
-     })
-     profile.forEach(vidprofile=>{
-  vidprofile.style.width="24vw"
-     })
-     timestmp.forEach(time=>{
-      time.style.left ="22vw";
-      time.style.top="11vw"
-     })
-     
+    container.style.width = "90vw";
+    thumbnails.forEach((thumbnail) => {
+      thumbnail.style.width = "26vw";
+    });
+    profile.forEach((vidprofile) => {
+      vidprofile.style.width = "24vw";
+    });
+    timestmp.forEach((time) => {
+      time.style.left = "22vw";
+      time.style.top = "11vw";
+    });
   }
 });
 
-
 // filter button Color js
-let filterButtons = document.querySelectorAll('.filter-btns button');
-let thumbnailcon = document.querySelectorAll('.container .thumbnail-container');
-let thumbnailbann = document.querySelector('.container .banner');
+let filterButtons = document.querySelectorAll(".filter-btns button");
+let thumbnailcon = document.querySelectorAll(".container .thumbnail-container");
+let thumbnailbann = document.querySelector(".container .banner");
 
-const filterThumbnail = e =>{
-  document.querySelector('.active').classList.remove('active');
-  e.target.classList.add('active');
-thumbnailbann.classList.add("hide");
-  thumbnailcon.forEach(thumb=>{
+const filterThumbnail = (e) => {
+  document.querySelector(".active").classList.remove("active");
+  e.target.classList.add("active");
+  thumbnailbann.classList.add("hide");
+  thumbnailcon.forEach((thumb) => {
     thumb.classList.add("hide");
 
-    if(thumb.dataset.name === e.target.dataset.name || e.target.dataset.name==='all'){
+    if (
+      thumb.dataset.name === e.target.dataset.name ||
+      e.target.dataset.name === "all"
+    ) {
       thumb.classList.remove("hide");
     }
-  })
-}
+  });
+};
 
-filterButtons.forEach(button=>button.addEventListener('click',filterThumbnail))
+filterButtons.forEach((button) =>
+  button.addEventListener("click", filterThumbnail)
+);
+
+// shorts cut js
+
+let shortHidebutton = document.querySelector(".shortsHide");
+let ShortsForHide = document.querySelectorAll(".shorts");
+shortHidebutton.addEventListener("click", function () {
+  ShortsForHide.forEach((hide) => {
+    hide.style.display = "none";
+  });
+});
